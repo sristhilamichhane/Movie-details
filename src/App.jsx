eeec0da0dbb713cb77898caa6195dc0fc95eaf1c
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-12-26 19:15:02 +0545

    feat: :sparkles: App.jsx for the task

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Movie from "./components/Movie";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-gray-400">
+      <Movie /> 
+    </div>
+  );
+};
+
+export default App;
