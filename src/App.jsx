8bec0a4193ff573f4287a348506eab3f87000ab8
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-12-26 20:19:33 +0545

    feat: :sparkles: More modification

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Movie from "./components/Movie";
 
 const App = () => {
   return (
-    <div className="h-screen w-full bg-gray-400">
-      <Movie /> 
+    <div className="h-full w-full bg-gray-400 text-white">
+      <h1 className="text-xl flex justify-center pt-4">Movie-Details</h1>
+      <Movie />
     </div>
   );
 };
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -11,21 +11,59 @@ const Movie = () => {
       )
       .then((res) => {
         console.log(res.data);
-        setMovieList(res.data.results);
+        setMovieList(
+          res.data.results.map((movie) => ({ ...movie, selectedStatus: null }))
+        );
       });
   };
+
+  const handleStatusChange = (movieId, newStatus) => {
+    setMovieList((prevMovies) =>
+      prevMovies.map((movie) =>
+        movie.id === movieId ? { ...movie, selectedStatus: newStatus } : movie
+      )
+    );
+  };
+
   useEffect(() => {
     fetchMovies();
   }, []);
 
   return (
-    <div className="flex flex-row gap-5 p-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-5">
       {movielist.map((movie) => (
-        <img
-          key={movie.id}
-          className="h-[25rem] w-[50rem] object-cover rounded-lg"
-          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-        />
+        <div key={movie.id} className="bg-white rounded-lg overflow-hidden">
+          <img
+            className="h-[20rem] w-full object-cover rounded-t-lg"
+            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            alt={movie.original_title}
+          />
+          <div className="p-3">
+            <h3 className="text-lg font-semibold mb-1 text-black">
+              {movie.original_title.length > 30
+                ? `${movie.original_title.slice(0, 30)}...`
+                : movie.original_title}
+            </h3>
+            <div className="flex justify-between mb-2">
+              <span className="text-gray-500">{movie.original_language}</span>
+            </div>
+            <div className="flex space-x-2 my-2">
+              {["Watching", "Completed", "Hold List"].map((status) => (
+                <button
+                  key={status}
+                  onClick={() => handleStatusChange(movie.id, status)}
+                  className={`${
+                    movie.selectedStatus === status
+                      ? `bg-${status.toLowerCase()}-500 text-green-500 px-3 py-1 rounded-md`
+                      : `bg-${status.toLowerCase()}-300 text-red-500 text-${status.toLowerCase()}-500 px-3 py-1 rounded-md`
+                  }`}
+                >
+                  {status}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
