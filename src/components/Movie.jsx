cb10ae1129dcca15fa789e73fc39ad2b0551f3cf
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-12-27 17:51:42 +0545

    feat: :sparkles: Hover effect on image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -32,18 +32,34 @@ const Movie = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-5">
       {movielist.map((movie) => (
-        <div key={movie.id} className="bg-white rounded-lg overflow-hidden">
-          <img
-            className="h-[20rem] w-full object-cover rounded-t-lg"
-            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-            alt={movie.original_title}
-          />
+        <div
+          key={movie.id}
+          className="bg-white rounded-lg overflow-hidden relative"
+        >
+          <div className="image-container relative">
+            <img
+              className="h-[20rem] w-full object-cover rounded-t-lg transition-transform transform hover:scale-105"
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.original_title}
+            />
+            <div className="overlay absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-80">
+              <div className="p-3 text-white">
+                <h3 className="text-lg font-semibold mb-1">
+                  {movie.original_title.length > 30
+                    ? `${movie.original_title.slice(0, 30)}...`
+                    : movie.original_title}
+                </h3>
+                <div className="flex items-center mb-1">
+                  <span className="mr-1 text-yellow-400">⭐</span>
+                  <p className="text-lg">{movie.vote_average}</p>
+                </div>
+                <p>
+                  Genre: {movie.genres?.map((genre) => genre.name).join(", ")}
+                </p>
+              </div>
+            </div>
+          </div>
           <div className="p-3">
-            <h3 className="text-lg font-semibold mb-1 text-black">
-              {movie.original_title.length > 30
-                ? `${movie.original_title.slice(0, 30)}...`
-                : movie.original_title}
-            </h3>
             <div className="flex justify-between mb-2">
               <span className="text-gray-500">{movie.original_language}</span>
             </div>
